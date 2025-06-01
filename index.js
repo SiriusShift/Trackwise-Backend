7891ce118e8ffbe6c34728297eb7be5cac2ce4bc
SiriusShift <lagmanmarquez@gmail.com>
2025-06-01 21:14:11 +0800

    updated expense model

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
-require("./config/cron.js")
+require("./config/cron.js");
 // Configs
-const {runDb} = require('./config/runDb.js');
-const {corsConfig} = require('./config/corsConfig.js');
-const {runRouters} = require("./routes/index.js");
-const {sessionConfig} = require('./config/sessionConfig.js');
-const {runPassport} = require('./config/passport.js');
+const { runDb } = require("./config/runDb.js");
+const { corsConfig } = require("./config/corsConfig.js");
+const { runRouters } = require("./routes/index.js");
+const { sessionConfig } = require("./config/sessionConfig.js");
+const { runPassport } = require("./config/passport.js");
 
 const app = express();
 const port = 5000;
